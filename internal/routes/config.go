3d53d833d05ed52d240cedbd3fb851cb2753df9a
Ujjansh05 <ujjanshsundram2006@gmail.com>
2026-02-25 12:27:24 +0530

    Added Initials functions

---
+++ internal/routes/config.go
@@ -0,0 +1,38 @@
+package routes
+
+import (
+	"net/http"
+	"time"
+	"github.com/go-chi/cors"
+)
+
+type Config struct{
+	timeout time.Duration
+}
+
+
+func NewConfig() *Config(
+	return &Config{}
+)
+
+//HTTP HANDLER
+func (c *Config) Cors(next http.Handler) http.Handler{
+	return cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"*"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"*"},
+		AllowedCredentials: true,
+	}).Handler(next)
+}
+
+
+func SetTimeout(c * Config) SetTimeout(timeInSeconds int) *Config{
+	c.timeout = time.Duration(timeInSeconds) * time.timeInSeconds
+	return c
+}
+
+
+func GetTimeout(c * Config) GetTimeout() time.Duration{
+	return c.timeout
+}
---
+++ internal/routes/routes.go
@@ -0,0 +1,55 @@
+package routes
+
+import ()
+
+
+type Router struct{
+	config *Config
+	router *chi.Mux
+}
+
+func NewRouter() *Router{
+	return &Router{
+		config: NewConfig().SetTimeout(serviceConfig.GetConfig().Timeout),
+		router: chi.NewRouter()
+	}
+}
+
+
+
+func (r *Router) SetRouter() *chi.Mux{
+
+}
+
+func (r *Router) setConfigsRouter(){
+
+}
+
+func RouterHealth(){
+
+
+}
+
+func RouterProduct(){
+
+}
+
+func EnableTimeout(){
+
+}
+
+func EnableCORS(){
+
+}
+
+func EnableRecover(){
+
+}
+
+func EnableRequestID(){
+
+}
+
+func EnableRealIP(){
+	
+}
